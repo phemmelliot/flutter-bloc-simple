5fa07f2d555841231f43c3042f113556b961c9a6
phemmelliot <adegokefemi17@gmail.com>
2019-06-22 01:29:06 +0100

    Add blocs and Events

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'counter_event.dart';
+
+class CounterBloc {
+  int _counter = 0;
+
+  // These are controllers for both the event incoming into the bloc and the state that is getting outputted
+  final _counterStateController = StreamController<int>();
+  final _counterEventController = StreamController<CounterEvent>();
+
+  // A state of type int gets inputted into this sink from the event controller's stream
+  StreamSink<int> get _counterStateInput => _counterStateController.sink;
+
+  // This is the stream that gets outputted to the view, it get its value from the sink above
+  Stream<int> get counterStateOuput => _counterStateController.stream;
+
+  // This is the sink where events get passed in from the view, it passes this down to the event controller's stream
+  Sink<CounterEvent> get counterEventInput => _counterEventController.sink;
+
+  CounterBloc() {
+    // This stream gets the event that get passed in from the event controller sink above
+    _counterEventController.stream.listen(_handleStateFromEvent);
+  }
+
+  // This is the function that manipulates the state based on the event and the adds the new state to the controller
+  _handleStateFromEvent(CounterEvent counterEvent) {
+    if (counterEvent is IncrementEvent){
+     _counter++;
+    } else {
+      _counter--;
+    }
+
+    _counterStateInput.add(_counter);
+  }
+
+  // This closes the streams to prevent memory leaks
+  void dispose() {
+    _counterEventController.close();
+    _counterStateController.close();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+import 'counter_event.dart';
+import 'counter_bloc.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _bloc = CounterBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: StreamBuilder(
+        stream: _bloc.counterStateOuput,
+        builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '${snapshot.data}',
+                  style: Theme.of(context).textTheme.display1,
+                ),
+              ],
+            ),
+          );
+        }
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () => _bloc.counterEventInput.add(IncrementEvent()),
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: () => _bloc.counterEventInput.add(DecrementEvent()),
+            tooltip: 'Decrement',
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bloc.dispose();
+  }
+}
